c0fed06c0be2353a264dc9c10dfaa97a723c745b
adem kalfalı <ademkalfa9@gmail.com>
2022-11-18 12:01:24 +0300

    redux_project

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import authSlice from "../features/authSlice";
+
+const PrivateRouter = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  return user?.email ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRouter;
